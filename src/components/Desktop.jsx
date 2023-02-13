211335828e161072c6cb39109ed8369eebe934a6
Vishwajith H Shastry <vishwajithhh12345@gmail.com>
2023-02-14 02:04:51 +0530

    Initial commit

---
+++ src/components/Desktop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Desktop() {
+  return (
+    <div className="desktop">
+      <div></div>
+    </div>
+  );
+}
+
+export default Desktop;
---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Start() {
+  return (
+    <div className="start">
+      <div></div>
+    </div>
+  );
+}
+
+export default Start;
